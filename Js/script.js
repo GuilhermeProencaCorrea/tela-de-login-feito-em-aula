c1500cf2998b293611a75b15c4ce34ab5c53e8f6
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-08-09 21:40:25 -0300

    :tada: Inicio do exercício

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+let email = document.getElementById("email");
+let senha = document.getElementById("senha");
+let btnEntar = document.getElementById("button-login");
+
+btnEntar.addEventListener("click", () => {
+    alert("Alerta!")
+    let emailG = email.value;
+
+    window.open()
+})
