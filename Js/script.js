967d973bb1a1773671b2c21572bbdbf5edf3fa10
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-08-14 03:44:22 -0300

    Evento entrar

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,9 @@
-let email = document.getElementById("email");
-let senha = document.getElementById("senha");
-let btnEntar = document.getElementById("button-login");
-
-btnEntar.addEventListener("click", () => {
-    alert("Alerta!")
-    let emailG = email.value;
-
-    window.open()
-})
+function loginEntra () {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    if(senha== 123 && email=='exemplo@gmail.com'){
+        window.open("home.html", "_self");
+    }else{
+        alert("Senha ou e-mail incorretos!");
+    }
+}
