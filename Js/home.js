33c1c6400f969c820cc6b1b573f50af32d3a2125
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-09-09 18:05:38 -0300

    Construção da tabela cadastro de produto e alguns ajustes

--- Js/home.js
+++ Js/home.js
@@ -2,5 +2,6 @@ const changeThemeBtn = document.querySelector("#change-theme")
 
 changeThemeBtn.addEventListener("change", function() {
     document.body.classList.toggle("dark");
-    
+    document.getElementById("table").classList.toggle("table-dark");
+
 });
