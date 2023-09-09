edb41eaf32bbffa7902c9543edb5d5d1d8bd502c
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-09-09 20:33:50 -0300

    :sparkles: Começando a trabalhar com a api

---
+++ Js/cadastro-de-produto.js
@@ -0,0 +1,3 @@
+const URL = '';
+
+let tabelaProduto = document.querySelector("");
--- Js/home.js
+++ Js/home.js
@@ -1,7 +1,7 @@
+// Dark Mode
 const changeThemeBtn = document.querySelector("#change-theme")
 
 changeThemeBtn.addEventListener("change", function() {
     document.body.classList.toggle("dark");
     document.getElementById("table").classList.toggle("table-dark");
-
 });
